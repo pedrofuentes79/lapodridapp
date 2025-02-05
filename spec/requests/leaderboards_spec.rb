704eb4f3669b4a0f550081fa3ba63f2064f90d98
pfuentes <pedrofuentes7799@gmail.com>
2025-02-05 16:56:46 -0300

    Adds the /api to the leaderboard endpoint

--- spec/requests/leaderboards_spec.rb
+++ spec/requests/leaderboards_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Leaderboards", type: :request do
-  describe "GET /games/:id/leaderboard" do
+  describe "GET /api/games/:id/leaderboard" do
     let!(:game) { Game.new([ "Pedro", "Auro", "Leon" ], { "1, 4" => "trump" }) }
 
     it "returns http success" do
-      get "/games/#{game.id}/leaderboard"
+      get "/api/games/#{game.id}/leaderboard"
       expect(response).to have_http_status(:success)
     end
   end
