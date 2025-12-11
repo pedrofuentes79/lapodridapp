d76b6ba2c2a39faa86f91281a61e201a42f8c2bd
Pedro Fuentes <pedrofuentes7799@gmail.com>
2025-12-11 20:25:52 -0300

    adds more tests that simulate situations of overwriting rounds

--- app/models/round.rb
+++ app/models/round.rb
@@ -6,44 +6,58 @@ class Round < ApplicationRecord
 
   def forbidden_number
     return nil unless bids.where.not(predicted_tricks: nil).count == game.players.count - 1
-    cards_dealt - total_asked_bids
+    cards_dealt - total_asked_tricks
   end
 
-  def total_asked_bids
+  def total_asked_tricks
     bids.sum(:predicted_tricks)
+  end
+
+  def total_made_tricks
+    bids.sum(:actual_tricks)
   end
 
   def points_for_player(player)
     bids.find_by(player: player).points
   end
 
+  # this method allows the player to rewrite its previous bid
   def player_asks_for_tricks(player, number_of_tricks)
-    bid = bids.find_by(player: player)
-    if bid.predicted_tricks.present?
-      raise "Player #{player.name} has already asked for tricks"
-    end
-    if number_of_tricks > cards_dealt
-      raise "Player #{player.name} can't ask for more tricks than the number of cards dealt"
-    end
-    if number_of_tricks == forbidden_number
-      raise "Player #{player.name} can't ask for #{forbidden_number} tricks"
-    end
+    bid = bid_for(player)
+    validate_tricks_within_cards_dealt!(player, number_of_tricks)
+    validate_forbidden_number!(player, number_of_tricks)
+
+    # if the player has already asked for tricks, we need to validate that the new bid makes the total number of tricks consistent
+    validate_overwrite_maintains_total_asked_tricks!(player, bid, number_of_tricks) if all_players_have_asked_for_tricks?
 
     bid.update(predicted_tricks: number_of_tricks)
   end
 
+
+  # this method allows the player to rewrite its previous `actual_tricks`
   def player_makes_tricks(player, number_of_tricks)
-    bid = bids.find_by(player: player)
-    
-    if bid.predicted_tricks.nil?
-      raise "Player #{player.name} hasn't asked for tricks yet"
-    end
-    
-    # this allows to overwrite the `made_tricks` value if needed
-    if number_of_tricks > cards_dealt
-      raise "Player #{player.name} can't make more tricks than the number of cards dealt"
-    end
+    bid = bid_for(player)
+
+    validate_player_has_asked_for_tricks!(player, bid)
+    validate_tricks_within_cards_dealt!(player, number_of_tricks)
+    validate_last_player_tricks!(player, number_of_tricks) if is_last_player_to_make_tricks(player)
+    validate_overwrite_maintains_total!(player, bid, number_of_tricks) if complete?
+
     bid.update(actual_tricks: number_of_tricks)
+  end
+
+  def is_last_player_to_make_tricks(player)
+    # only one bid remaining to be made, and it's for this player
+    bids_remaining = bids.where(actual_tricks: nil)
+    bids_remaining.count == 1 and bids_remaining.first.player == player
+  end
+
+  def all_players_have_asked_for_tricks?
+    bids.where(predicted_tricks: nil).count == 0
+  end
+
+  def bid_for(player)
+    bids.find_by(player: player)
   end
 
   def complete?
@@ -51,6 +65,43 @@ class Round < ApplicationRecord
   end
 
   private
+
+  def validate_player_has_asked_for_tricks!(player, bid)
+    raise "Player #{player.name} hasn't asked for tricks yet" if bid.predicted_tricks.nil?
+  end
+
+  def validate_forbidden_number!(player, number_of_tricks)
+    raise "Player #{player.name} can't ask for #{forbidden_number} tricks" if number_of_tricks == forbidden_number
+  end
+
+  def validate_tricks_within_cards_dealt!(player, number_of_tricks)
+    raise "Invalid number of tricks" if number_of_tricks > cards_dealt
+  end
+
+  def validate_last_player_tricks!(player, number_of_tricks)
+    remaining_tricks = cards_dealt - total_made_tricks
+    if number_of_tricks != remaining_tricks
+      raise "Player #{player.name} can only make #{remaining_tricks} tricks"
+    end
+  end
+
+  def validate_overwrite_maintains_total!(player, bid, number_of_tricks)
+    old_tricks = bid.actual_tricks
+    new_total = total_made_tricks - old_tricks + number_of_tricks
+
+    if new_total != cards_dealt
+      raise "Player #{player.name} can only make #{cards_dealt - total_made_tricks + old_tricks} tricks"
+    end
+  end
+
+
+  def validate_overwrite_maintains_total_asked_tricks!(player, bid, number_of_tricks)
+    old_tricks = bid.predicted_tricks
+    new_total = total_asked_tricks - old_tricks + number_of_tricks
+
+    # this is because the total number of asked tricks can never be equal to the number of cards dealt
+    raise "Player #{player.name} can't ask for #{number_of_tricks} tricks" if new_total == cards_dealt
+  end
 
   def create_bids_for_players
     game.players.each do |player|
--- test/models/round_test.rb
+++ test/models/round_test.rb
@@ -40,15 +40,15 @@ class RoundTest < ActiveSupport::TestCase
 
   test "should know total points for a player" do
     round = @game.rounds.create(cards_dealt: 7, round_number: 1)
-    round.player_asks_for_tricks(@player1, 2)
+    round.player_asks_for_tricks(@player1, 5)
     round.player_asks_for_tricks(@player2, 3)
-    round.player_asks_for_tricks(@player3, 1)
+    round.player_asks_for_tricks(@player3, 2)
     round.player_makes_tricks(@player1, 3) 
     round.player_makes_tricks(@player2, 2)
-    round.player_makes_tricks(@player3, 1)
+    round.player_makes_tricks(@player3, 2)
     assert_equal 3, round.points_for_player(@player1)
     assert_equal 2, round.points_for_player(@player2)
-    assert_equal 12, round.points_for_player(@player3)
+    assert_equal 14, round.points_for_player(@player3)
   end
 
   test "should raise error if player asks for more tricks than the number of cards dealt" do
@@ -56,7 +56,7 @@ class RoundTest < ActiveSupport::TestCase
     error = assert_raises(RuntimeError) do
       round.player_asks_for_tricks(@player1, 8)
     end
-    assert_equal "Player #{@player1.name} can't ask for more tricks than the number of cards dealt", error.message
+    assert_equal "Invalid number of tricks", error.message
   end
 
   test "should raise error if player asks for the forbidden number of tricks" do
@@ -79,5 +79,52 @@ class RoundTest < ActiveSupport::TestCase
     end
   end
 
+  test "should raise if the last player to make tricks doesn't make the total number of tricks" do
+    round = @game.rounds.create(cards_dealt: 7, round_number: 0)
+    @game.ask_for_tricks(round, @player1, 2)
+    @game.ask_for_tricks(round, @player2, 3)
+    @game.ask_for_tricks(round, @player3, 1)
+    @game.make_tricks(round, @player1, 2)
+    @game.make_tricks(round, @player2, 3)
+    _ = assert_raises(RuntimeError) do
+      @game.make_tricks(round, @player3, 3)
+    end
+  end
 
+  test "should allow player to overwrite the tricks they asked for" do
+    round = @game.rounds.create(cards_dealt: 7, round_number: 0)
+    @game.ask_for_tricks(round, @player1, 2)
+    @game.ask_for_tricks(round, @player2, 3)
+    @game.ask_for_tricks(round, @player3, 1)
+    @game.ask_for_tricks(round, @player1, 1)
+  end
+
+  test "should allow last player to overwrite the tricks they asked for" do
+    round = @game.rounds.create(cards_dealt: 7, round_number: 0)
+    @game.ask_for_tricks(round, @player1, 2)
+    @game.ask_for_tricks(round, @player2, 3)
+    @game.ask_for_tricks(round, @player3, 1)
+    @game.ask_for_tricks(round, @player3, 3)
+  end
+  test "should not allow any player to overwrite the tricks they asked for if they ask for a forbidden number" do
+    round = @game.rounds.create(cards_dealt: 7, round_number: 0)
+    @game.ask_for_tricks(round, @player1, 2)
+    @game.ask_for_tricks(round, @player2, 3)
+    @game.ask_for_tricks(round, @player3, 1)
+
+    error = assert_raises(RuntimeError) do
+      @game.ask_for_tricks(round, @player3, 2)
+    end
+    assert_equal "Player #{@player3.name} can't ask for 2 tricks", error.message
+
+    error = assert_raises(RuntimeError) do
+      @game.ask_for_tricks(round, @player2, 4)
+    end
+    assert_equal "Player #{@player2.name} can't ask for 4 tricks", error.message
+
+    error = assert_raises(RuntimeError) do
+      @game.ask_for_tricks(round, @player1, 3)
+    end
+    assert_equal "Player #{@player1.name} can't ask for 3 tricks", error.message
+  end
 end
